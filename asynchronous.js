64ff62279d8b7a69e6edf7b21e2aa10fce25d2dd
imranbappy <imranhosenbappy690@gmail.com>
2021-04-25 20:45:10 +0600

    asynchronous done

--- asynchronous.js
+++ asynchronous.js
@@ -16,20 +16,68 @@ function programmer(isProgrammer) {
     })
     return promise
 }
-// const programmerSummary = obb =>{
-//     return new Promise((resolve, reject)=>{
-//         resolve(`${`Hello ${obb.name} you ara `} a good ${obb.job}`)
-//     })
-// }
-
 const programmerSummary = obb =>{
-    return Promise.resolve(`${`Hello ${obb.name} you ara `} a good ${obb.job}`)
+    return new Promise((resolve, reject)=>{
+        resolve(`${`Hello ${obb.name} you ara `} a good ${obb.job}`)
+    })
 }
 
-programmer(true)
-    .then(programmerSummary)
-    .then(result=>{
-        console.log(result);
-    }).catch(err=>{
+async function asyncFun(prams){
+    try {
+        const pro = await programmer(prams)
+        const result = await programmerSummary(pro)
+        // console.log(result);
+        return result
+    } catch (err) {
         console.log(err.message);
-    })
+    }
+}
+
+// asyncFun(false)
+// .then((res)=>console.log(res))
+
+// const programmerSummary = obb =>{
+//     return Promise.resolve(`${`Hello ${obb.name} you ara `} a good ${obb.job}`)
+// }
+
+// programmer(true)
+//     .then(programmerSummary)
+//     .then(result=>{
+//         console.log(result);
+//     }).catch(err=>{
+//         console.log(err.message);
+//     })
+
+// const promise1 = Promise.resolve('This is promise1')
+
+// const promise1 = new Promise((resolve, reject)=>{
+//     setTimeout(() => {
+//         resolve('This is promise1')
+//     }, 400);
+// })
+
+// const promise2 = new Promise((resolve, reject)=>{
+//     setTimeout(() => {
+//         resolve('This is promise2')
+//     }, 400);
+// })
+
+// promise1.then(res=>console.log(res))
+// promise2.then(res=>console.log(res))
+
+// Promise.all([promise1, promise2]).then(res=>{
+//     console.log(res);
+// })
+
+// Promise.race([promise2, promise1]).then(res=>{
+//     console.log(res);
+// })
+
+
+// const friendlyFunction = async ()=>{
+//     const result = await promise1
+//     console.log(result);
+// }
+
+// friendlyFunction()
+
